12235869d841c8b6d87112f79f55747ff62ccd0d
Aluizio Damas <135044419+Alu1z1o-Damas@users.noreply.github.com>
2023-08-08 18:47:28 -0300

    atualização

---
+++ desafios/016.py
@@ -0,0 +1,10 @@
+'''import math
+   num = float(input('Digite um valor: '))
+   print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, math.trunc(num)))'''
+
+'''fron math import trunc
+   num = float(input('Digite um valor: '))
+   print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))'''
+
+num = float(input('Digite um valor: '))
+print('O valor digita foi {} e a sua porção inteira é {}'.format(num, int(num)))
